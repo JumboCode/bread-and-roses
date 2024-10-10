b40eb39c5af49701234f3f0481c4a120999939bd
wkim10 <wkim10@tufts.edu>
2024-10-10 18:42:37 -0400

    update schema and route.ts

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -3,38 +3,14 @@ import { NextRequest, NextResponse } from "next/server";
 
 const prisma = new PrismaClient();
 
-/* TODO: Need to fix */
-export const GET = async (request: NextRequest) => {
-  try {
-    const { id } = await request.json();
-
-    const user = await prisma.user.findUnique({
-      where: { id: id },
-    });
-
-    return NextResponse.json({
-      code: "SUCCESS",
-      message: user,
-    });
-  } catch (error) {
-    console.error("Error:", error);
-    return NextResponse.json({
-      code: "ERROR",
-      message: error,
-    });
-  }
-};
-
 export const POST = async (request: NextRequest) => {
   try {
+    /* @TODO: Add auth */
+
     const { user } = await request.json();
 
     const savedUser = await prisma.user.create({
-      data: {
-        firstName: user.firstName,
-        lastName: user.lastName,
-        email: user.email,
-      },
+      data: user,
     });
 
     return NextResponse.json({
