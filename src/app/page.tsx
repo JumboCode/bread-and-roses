4973b8f3a875ce66ff2654cf2a4ed3562a352145
Jonah Pflaster <jonahpflaster23pj@gmail.com>
2024-10-18 20:46:52 -0400

    added the 4 main variables for the card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,12 @@ import StatsCard from "@components/StatsCard";
 export default function Home() {
   return (
     <div className="flex items-center justify-center h-screen">
-      <StatsCard/>
+      <StatsCard
+      heading = "Total Voluteers"
+      value = "50"
+      icon = "formkit:people" //icon id
+      date = "October 5, 2024"
+      />
     </div>
   );
 }
--- src/components/StatsCard.tsx
+++ src/components/StatsCard.tsx
@@ -1,8 +1,23 @@
-const StatsCard = () => {
+import React from 'react';
+import { Icon } from "@iconify/react/dist/iconify.js";
+
+interface StatsCardProps {
+  heading: string;
+  value: number | string;
+  date: string;
+  icon: string;
+}
+
+const StatsCard = ({ heading, value, icon, date }: StatsCardProps) => {
   return (
-    <div className="max-w-sm p-36 bg-white border border-gray-200 rounded-lg shadow dark:bg-white-800 dark:border-gray-700">
-        <div className ="p-5">
-            <p className="mb-7 font-style italic text-black dark:text-black">Total Volunteers</p>
+    <div className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-lg">
+        <div className ="flex flex-col justify-between">
+            <h2 className="font-style font-bold text-black">{heading}</h2>
+            <p className="font-style font-bold text-black text-6xl">{value}</p>
+            <div className="text-right">
+              <Icon icon={icon} className="text-5xl" style={{ color: '#33BDB5' }} />
+            </div>
+            <p className="font-style text-gray-200">{date}</p>
         </div>
     </div>
   );
