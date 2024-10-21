4b139f39f79487933bb04d6e9b3b853073f3613e
ManuP6789 <manuel.pena654576@tufts.edu>
2024-10-20 21:28:11 -0400

    create user

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -7,18 +7,21 @@ export const POST = async (request: NextRequest) => {
   try {
     /* @TODO: Add auth */
 
-    const { User, VolunteerDetails } = await request.json();
+    const { user, VolunteerDetails } = await request.json();
 
     const savedUser = await prisma.user.create({
       data: {
-        User,
-        volunteerDetails: {
-          create: {
-            VolunteerDetails,
-          },
-        },
+        ...user, // Ensure User contains properties needed to create a User
       },
     });
+
+    await prisma.volunteerDetails.create({
+      data: {
+        ...VolunteerDetails,
+        userId: savedUser.id, // Link to the newly created User's ID
+      },
+    });
+
     return NextResponse.json({
       code: "SUCCESS",
       message: savedUser.email,
