d3fbea5cd2923c5cc1e137335a4dffa90706b4f6
KY233466 <zihan.yang@tufts.edu>
2024-10-25 14:09:00 -0400

    add border and shadow

--- src/components/VolunteerTable/VolunteerTable.tsx
+++ src/components/VolunteerTable/VolunteerTable.tsx
@@ -1,11 +1,9 @@
-import * as React from "react";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
 import Avatar from "@mui/material/Avatar";
 import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
 import ArrowRightAltIcon from "@mui/icons-material/ArrowRightAlt";
@@ -22,18 +20,39 @@ function createData(
   return { name, type, email, location };
 }
 
+function getUserRole(userType: number): string {
+  switch (userType) {
+    case 0:
+      return "Corporate Team";
+    case 1:
+      return "Community Group";
+    case 2:
+      return "Individual Volunteer";
+    default:
+      return "undefined user role"
+  }
+}
+
 const rows = [
   createData("Name1", 0, "email1", "location1"),
   createData("Name2", 1, "email2", "location2"),
   createData("Name3", 2, "email3", "location2"),
-  createData("Name4", 3, "email4", "location3"),
 ];
 
 export default function VolunteerTable() {
   return (
-    <TableContainer component={Paper}>
+    <TableContainer
+      sx={{
+        border: "solid 1px #E4E7EC",
+        borderRadius: "12px",
+        boxShadow: "0px 2px 4px -2px #1018280F, 0px 4px 8px -2px #1018281A",
+      }}
+    >
       <Table
-        sx={{ minWidth: 650, borderColor: "#E4E7EC" }}
+        sx={{
+          minWidth: 650,
+          borderColor: "#E4E7EC",
+        }}
         aria-label="simple table"
       >
         <TableHead>
@@ -74,7 +93,8 @@ export default function VolunteerTable() {
               <TableCell
                 sx={{
                   borderColor: "#E4E7EC",
-                  color: "#344054",
+                  fontWeight: "bold",
+                  color: "#101828",
                   display: "flex",
                   flexDirection: "row",
                   alignItems: "center",
@@ -90,7 +110,7 @@ export default function VolunteerTable() {
                 sx={{ borderColor: "#E4E7EC", color: "#344054" }}
                 align="left"
               >
-                {row.type}
+                {getUserRole(row.type)}
               </TableCell>
               <TableCell
                 sx={{ borderColor: "#E4E7EC", color: "#344054" }}
