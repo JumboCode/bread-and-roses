6cf6273567286b3a4aea84805fab4934be084e26
wkim10 <wkim10@tufts.edu>
2024-10-29 12:30:57 -0400

    small fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
 "use client";
 
-import TopHeader from "@components/TopHeader";
-
 export default function Home() {
-  return (
-    <div className="flex items-center justify-center h-screen">
-      <TopHeader userType="admin" />
-    </div>
-  );
+  return <div className="flex items-center justify-center h-screen"></div>;
 }
