6611cdd2ec41a3c1689fc701bfed725482043f0f
ManuP6789 <manuel.pena654576@tufts.edu>
2024-11-08 14:13:28 -0500

    clean code

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -24,22 +24,17 @@ export const options: NextAuthOptions = {
         if (!credentials) {
           return null; // or throw an error
         }
-        console.log("Trying to authorize");
         const { email, password } = credentials;
         // This is where you need to retrieve user data
         // to verify with credentials
         // Docs: https://next-auth.js.org/configuration/providers/credentials
-        console.log("FETCHING USER:");
 
         const user: User = await getUserByEmailServer(email);
 
         // Check if user exists and if password matches
-        console.log("PRINTING:", user.password);
         if (user && (await compare(password, user.password))) {
-          console.log("SUCCESFUL");
           return user; // Authentication successful
         } else {
-          console.log("FAIL");
           return null; // Authentication failed
         }
       },
