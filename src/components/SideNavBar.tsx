96405c5ed351fcd748be3186df44183cb6843f22
vle04 <ngocvinale@gmail.com>
2024-11-11 21:07:04 -0500

    working on pr comments
    began working with the next.js router

--- src/components/SideNavBar.tsx
+++ src/components/SideNavBar.tsx
@@ -1,10 +1,25 @@
+"use client"
 import { Icon } from "@iconify/react/dist/iconify.js";
+import { useRouter } from "next/navigation";
 import Image from "next/image";
 import Logo from "../app/icons/br-logo.png";
-import Communicaton from "../app/icons/communication";
 import Divider from "../app/icons/divider";
 
+// TODO: routing to different pages (make the pages)
+// note that the highlight of the button goes away when u click off it!
+
 const SideNavBar = () => {
+  const router = useRouter();
+
+  const tabs = [
+    { name: 'Home', href: '/' },
+    { name: 'Events', href: '/events' },
+    { name: 'Volunteers', href: '/volunteers' },
+    { name: 'Communication', href: '/communication' },
+    { name: 'Profile', href: '/profile' },
+    { name: 'Logout', href: '/logout' },
+  ];
+  
   return (
     <div className="h-screen w-60 border border-gray-200 fixed left-0">
       <div className="flex place-content-center my-6 ">
@@ -12,41 +27,53 @@ const SideNavBar = () => {
       </div>
 
       <nav className="flex items-center gap-1 flex-1 px-6 text-lg">
+
+        {/* {tabs.map((tab) => (
+          <Link href={tab.href} key={tab.name}>
+            <button className="nav-button flex gap-3 items-center h-11 text-[18px] font-normal font-medium w-full text-darkrose bg-rose rounded-md pt-px pb-px px-3">
+              <Icon icon="tabler:home" width="24" height="24" /> Home
+            </button>
+              {tab.name}
+            </button>
+          </Link>
+        ))} */}
+
+        
         <ul>
           <li>
-            <button className="nav-button flex gap-3 items-center h-11 text-[18px] font-normal font-medium w-full text-darkrose bg-rose rounded-md pt-px pb-px px-3">
+            <button type="button" className="nav-button flex gap-3 items-center h-11 text-[18px] font-normal font-medium w-full focus:text-darkrose focus:bg-rose rounded-md pt-px pb-px px-3" onClick={() => router.replace('/')}>
               <Icon icon="tabler:home" width="24" height="24" /> Home
             </button>
           </li>
 
           <li>
-            <button className="nav-button flex gap-3 items-center h-11 text-[18px] font-normal font-medium w-full rounded-md pt-px pb-px px-3">
+            <button className="nav-button flex gap-3 items-center h-11 text-[18px] font-normal font-medium w-full focus:text-darkrose focus:bg-rose rounded-md pt-px pb-px px-3" onClick={() => router.replace('/')}>
               <Icon icon="uil:calender" width="24" height="24" /> Events
             </button>
           </li>
 
           <li>
-            <button className="nav-button flex gap-3 items-center h-11 text-[18px] font-normal font-medium w-full rounded-md pt-px pb-px px-3">
+            <button className="nav-button flex gap-3 items-center h-11 text-[18px] font-normal font-medium w-full focus:text-darkrose focus:bg-rose rounded-md pt-px pb-px px-3" onClick={() => router.replace('/')}>
               <Icon icon="pepicons-print:people" width="24" height="24" />{" "}
               Volunteers
             </button>
           </li>
 
           <li>
-            <button className="nav-button flex gap-3 items-center h-11 text-[18px] font-normal font-medium w-full rounded-md pt-px pb-px px-3">
-              <Communicaton />
+            <button className="nav-button flex gap-3 items-center h-11 text-[18px] font-normal font-medium w-full focus:text-darkrose focus:bg-rose rounded-md pt-px pb-px px-3" onClick={() => router.replace('/')}>
+            <Icon icon="material-symbols:inbox-outline" width="24" height="24" /> 
               Communication
             </button>
           </li>
 
           <li>
-            <button className="nav-button flex gap-3 items-center h-11 text-[18px] font-normal font-medium w-full rounded-md pt-px pb-px px-3">
+            <button className="nav-button flex gap-3 items-center h-11 text-[18px] font-normal font-medium w-full focus:text-darkrose focus:bg-rose rounded-md pt-px pb-px px-3" onClick={() => router.replace('/')}>
               <Icon icon="charm:person" width="24" height="24" /> Profile
             </button>
           </li>
 
           <li>
-            <button className="nav-button flex gap-3 items-center h-11 text-[18px] font-normal font-medium w-full rounded-md pt-px pb-px px-3">
+            <button className="nav-button flex gap-3 items-center h-11 text-[18px] font-normal font-medium w-full focus:text-darkrose focus:bg-rose rounded-md pt-px pb-px px-3" onClick={() => router.replace('/')}>
               <Icon icon="tabler:logout" width="24" height="24" /> Logout
             </button>
           </li>
