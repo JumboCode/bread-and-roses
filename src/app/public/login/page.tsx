60b494efe92ab4a4cfb6b0fc770fd42a8e01f1c8
KY233466 <81402259+KY233466@users.noreply.github.com>
2024-11-16 02:34:51 +0800

    Frontend log in flow (#28)
    * added stuff
    
    * set up
    
    * change structure
    
    * changes
    
    * change to mui
    
    * add error handlings
    
    * Update Login.tsx
    
    * make password not show initially
    
    * fix error
    
    * change icon color
    
    * Update logo1.png
    
    * some changes
    
    ---------
    
    Co-authored-by: jpaik03 <justin.paik@tufts.edu>

---
+++ src/app/public/login/page.tsx
@@ -0,0 +1,11 @@
+import LoginForm from "@components/Login";
+
+const Login = () => {
+  return (
+    <div style={{padding: '3rem'}}>
+      <LoginForm />
+    </div>
+  );
+};
+
+export default Login;
