dca345799e6da8d18888b0fdb94afef9b8f19cc1
jmill-16 <159972742+jmill-16@users.noreply.github.com>
2024-11-15 14:24:09 -0500

    13 dashboard alert (#32)
    * structure for dashboard message
    
    border plus icon, need to make it white though
    
    * added text
    
    need to work on spacing
    
    * added close tab icon
    
    * fixed text issue
    
    text extended out past borders of box and would not wrap around. Added break-words and removed box height to allow box to extend down if needed

---
+++ src/components/DashboardAlert.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Icon } from "@iconify/react/dist/iconify.js";
+
+interface DashboardAlertProps {
+  Title: string;
+  Subtext: number | string;
+}
+
+const DashboardAlert = ({ Title, Subtext }: DashboardAlertProps) => {
+    return (
+        <div className="ml-8 flex items-start relative">
+            {/* Side Tab */}
+            <div className="flex shadow-md w-[40px] py-0 px-[8px] items-center gap-[10px] self-stretch rounded-tl-[8px] rounded-bl-[8px] bg-[#FF8B14]">
+                <Icon icon="lucide:info" width="24" height="24" style={{ color: 'white' }} />
+            </div>
+            
+            {/* Main Content Box */}
+            <div className="flex flex-col shadow-md w-[1094px] p-[24px] px-[28px] items-start gap-[16px] rounded-tr-[8px] rounded-br-[8px] border border-[#FF8B14] bg-[#FFF] relative">
+                <div className="pr-[32px] text-[#101828] text-lg font-bold font-['Sofia Pro'] leading-normal break-words">
+                    {Title}
+                </div>
+                <div className="text-[#344054] text-base font-normal font-['Sofia Pro'] leading-normal break-words">
+                    {Subtext}
+                </div>
+
+                {/* Close Icon positioned in the top-right corner */}
+                <div className="absolute top-6 right-7">
+                    <Icon icon="icon-park-outline:close-one" width="24" height="24" style={{ color: 'grey/500' }} />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default DashboardAlert;
+
+
