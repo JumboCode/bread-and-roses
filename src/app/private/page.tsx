6ab22078e5c7929c9c955da871ec09b0a679b49e
wkim10 <wkim10@tufts.edu>
2024-12-11 05:15:55 -0500

    eslint thing

--- src/app/private/page.tsx
+++ src/app/private/page.tsx
@@ -3,7 +3,7 @@
 import { useSession } from "next-auth/react";
 
 export default function HomePage() {
-  const { data: session, status } = useSession();
+  const { data: session } = useSession();
 
   if (!session) {
     return <p>You are not logged in.</p>;
