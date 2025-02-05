2db5126df996b474f76d3b839bceda11d7f479db
vle04 <ngocvinale@gmail.com>
2025-02-05 18:50:30 -0500

    added searchbar
    edited searchbar component so that there's an option to customize the width

--- src/app/private/events/page.tsx
+++ src/app/private/events/page.tsx
@@ -4,11 +4,13 @@ import React from "react";
 import "react-day-picker/style.css";
 import { InitialCalendar } from "@components/InitalCalendar";
 import SearchBar from "@components/SearchBar";
-// import { EventCard } from "@components/EventCard";
+import EventCard from "@components/EventCard";
 
 export default function EventsPage() {
     const [selected, setSelected] = React.useState<string[]>([]);
     const [searchText, setSearchText] = React.useState("");
+    const [availability, setAvailability] = React.useState<"open" | "closed">(
+      "open");    
 
   return (
     <div>
@@ -18,13 +20,25 @@ export default function EventsPage() {
           setSearchText(value);
           setSelected([]);
         }}
+        width="100%"
       />
       <div className="text-center">
         <div className="relative w-full h-[50vh]">
           <InitialCalendar />
-          {/* <EventCard /> */}
+          
+          
+          {/* <EventCard
+            title="Community Cleanup"
+            start={new Date("2025-02-10T09:00:00")}
+            end={new Date("2025-02-10T12:00:00")}
+            address="123 Main Street, Boston, MA"
+            volunteers={10}
+            maxVolunteers={20}
+          /> */}
         </div>
       </div>
+
+      
     </div>
   );
 }
