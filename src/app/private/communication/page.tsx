2d7596e2f7bcb091a0af655469865ca3d8603010
tomasmaranga <tomas.maranga@icloud.com>
2025-02-06 19:10:08 -0500

    seeing if branch worked

--- src/app/private/communication/page.tsx
+++ src/app/private/communication/page.tsx
@@ -1,12 +1,52 @@
+"use client";
+
 import React from "react";
 import Image from "next/image";
+import { useId } from "react";
+import { useState } from "react";
+import { Role, User } from "@prisma/client";
+import { deleteUser, getUsersByRole } from "@api/user/route.client";
 
 export default function CommunicationPage() {
+  const postTextAreaId = useId();
+  const [text, setText] = useState("");
+  const [users, setUsers] = React.useState<User[]>();
+
+  React.useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await getUsersByRole(Role.VOLUNTEER);
+        setUsers(response.data);
+      } catch (error) {
+        console.error("Error fetching volunteers:", error);
+      }
+    };
+
+    fetchUsers();
+  }, []);
+
   return (
     <div>
       <div className="text-4xl font-['Kepler_Std'] font-semibold">
         Communication
       </div>
+      <div className="flex flex-col w-[800px]">
+        <label htmlFor={postTextAreaId}>Write your message:</label>
+        <textarea
+          className="border border-gray-500"
+          id={postTextAreaId}
+          name="postContent"
+          rows={4}
+          cols={40}
+          onChange={(e) => {
+            setText(e.target.value);
+          }}
+        />
+        <button className="w-[150px] font-semibold bg-teal-600 p-2.5 px-3 text-white rounded-md items-center">
+          Send
+        </button>
+      </div>
+
       <div className="text-center">
         <div className="relative w-full h-[50vh]">
           <Image
--- src/lib/nodemail.ts
+++ src/lib/nodemail.ts
@@ -1,4 +1,5 @@
 import nodemailer from "nodemailer";
+import { User } from "@prisma/client";
 
 export const sendMail = async (email: string, code: string) => {
   const transporter = nodemailer.createTransport({
@@ -23,3 +24,27 @@ export const sendMail = async (email: string, code: string) => {
 
   await transporter.sendMail(message);
 };
+
+// export const sendMassMail = async (users: User[], text: string) => {
+//   const transporter = nodemailer.createTransport({
+//     service: "gmail",
+//     auth: {
+//       user: process.env.NODEMAILER_EMAIL,
+//       type: "OAuth2",
+//       clientId: process.env.OAUTH_CLIENTID,
+//       clientSecret: process.env.OAUTH_CLIENTSECRET,
+//       refreshToken: process.env.OAUTH_REFRESHTOKEN,
+//       accessToken: process.env.OAUTH_ACCESSTOKEN,
+//     },
+//   });
+
+//   const massMessage = {
+//     from: process.env.NODEMAILER_EMAIL,
+//     to: email,
+//     subject: "Message from Bread & Roses Admin",
+//     text: text,
+//     html: `idk what this does`,
+//   };
+
+//   await transporter.sendMail(massMessage);
+// };
