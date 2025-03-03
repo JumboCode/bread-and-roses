9d1bdbc474248c306114aec78532c72441254532
vle04 <ngocvinale@gmail.com>
2025-03-02 21:23:18 -0500

    styling confirm shift check-in stage
    now replying on a useState to track what stage of the check-in flow the user is on. began styling the second stage of this flow (confirming shift check-in)

--- src/components/CheckInOut.tsx
+++ src/components/CheckInOut.tsx
@@ -28,6 +28,9 @@ export default function CheckInOutForm() {
   const [emailDisplayError, setEmailDisplayError] = useState(false);
   const [activeButton, setActiveButton] = useState<"checkin" | "checkout" | null>(null);
 
+  // use state to keep track of what stage of check-in flow user is on
+  const [stage, setStage] = useState<"shifts" | null>(null);
+
   // mock text for now until backend is implemented
   const emailOptions = [
     "example1@example.com",
@@ -38,25 +41,89 @@ export default function CheckInOutForm() {
   // submit logic
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-
-    // const res = await signIn("credentials", {
-    //   email,
-    //   redirect: false,
-    // });
-
-    // if (res?.error) {
-    //   if (res?.error == "Invalid password") {
-    //     setPasswordDisplayError(true);
-    //     setTimeout(() => setPasswordDisplayError(false), 3000);
-    //   } else if (res?.error == "Invalid user") {
-    //     setEmailDisplayError(true);
-    //     setTimeout(() => setEmailDisplayError(false), 3000);
-    //   }
-    // } else {
-    //   window.location.href = "/";
-    // }
   };
 
+  // checking in with shifts stage
+  if (stage === "shifts") {
+    return (
+      <div className="flex flex-col items-center w-full">
+      <div className="flex flex-col justify-center items-center min-h-screen w-full">
+        <Image src={logo1} alt="Logo" height={173} width={215} className="mb-[24px]"/>
+        <div className="p-6 border border-[#D0D5DD] rounded-[20px] shadow-[0px_8px_8px_-4px_#10182808,_0px_20px_24px_-4px_#10182814] flex justify-center items-start pt-6 w-1/2">
+          <div className="flex flex-col items-center w-full">
+            <div className="flex flex-col items-center">
+              <div
+                style={{
+                  color: "#9A0F28",
+                  fontSize: "36px",
+                  fontWeight: 600,
+                  fontFamily: "Kepler Std",
+                }}
+              >
+                Daily Check-In/Check-Out
+              </div>
+              <div className="text-[#667085] text-[16px] font-normal mb-[10px]">
+                Friday, January 31, 2025
+              </div>
+            </div>
+            <hr className="w-full border-t border-[#D0D5DD] mb-[20px]" />
+
+            <div className="flex flex-col w-full text-lg font-bold text-[#344054] gap-[32px]">
+              Here is your shift signup information:
+              <div className="text-[16px] font-bold text-[#344054]">
+                Name
+                <TextField
+                sx={{ width: "100%" }}
+                id="outlined-basic"
+                label=""
+                variant="filled"
+                />
+              </div>
+              
+              {/* TODO: implement mapping feature to display shifts */}
+              <div className="text-[16px] font-bold text-[#344054]">
+                Shift(s) (choose one)
+                <div className="flex flex-row gap-[16px] items-center mt-[8px]">
+                  <input
+                    id="shift"
+                    type="radio"
+                    className="size-[20px]"
+                  />
+                  <TextField
+                    sx={{ width: "50%" }}
+                    id="outlined-basic"
+                    label="Start"
+                    variant="filled"
+                  />
+                  <TextField
+                    sx={{ width: "50%" }}
+                    id="outlined-basic"
+                    label="End"
+                    variant="filled"
+                  />
+                </div>
+              </div>
+            </div>
+
+            <button
+              className="bg-[#138D8A] mt-[32px] text-white text-[16px] py-[10px] px-[18px] rounded-[8px] w-full text-center font-semibold"
+              type="submit"
+              // onClick={(e) => {
+              //   if (email !== "") {
+              //     handleSubmit(e);
+              //   }
+              // }}
+            >
+              Confirm Your Check-In
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+    );
+  }
+
+  // initial checking in stage
   return (
     <div className="flex flex-col items-center w-full">
       <div className="flex flex-col justify-center items-center min-h-screen w-full">
@@ -79,6 +146,7 @@ export default function CheckInOutForm() {
               </div>
             </div>
             <hr className="w-full border-t border-[#D0D5DD] mb-[20px]" />
+
             <div className="flex flex-col w-full text-lg font-bold text-[#344054] gap-[8px]">
                     I want to
                 <div className="flex flex-row gap-4 w-full">
@@ -131,6 +199,7 @@ export default function CheckInOutForm() {
               onClick={(e) => {
                 if (email !== "") {
                   handleSubmit(e);
+                  setStage("shifts");
                 }
               }}
             >
