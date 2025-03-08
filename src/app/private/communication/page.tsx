a977ec036f5f2219352cc723479fa6b1a0fc2c27
tomasmaranga <tomas.maranga@icloud.com>
2025-03-07 22:17:17 -0500

    first attempt at adding confirmation page

--- src/app/private/communication/page.tsx
+++ src/app/private/communication/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useEffect, useState, useRef } from "react";
-// import Image from "next/image";
+import Image from "next/image";
 import { sendMassEmail } from "@api/email/route.client";
 import useApiThrottle from "../../../hooks/useApiThrottle";
 import GroupRoundedIcon from "@mui/icons-material/GroupRounded";
@@ -11,6 +11,13 @@ import DeleteIcon from "@mui/icons-material/Delete";
 import { Attachment } from "nodemailer/lib/mailer";
 
 export default function CommunicationPage() {
+  const [step, setStep] = useState(2);
+
+  interface SendEmailResponse {
+    code: "SUCCESS" | "ERROR";
+    message: string;
+  }
+
   const [subject, setSubject] = React.useState("");
   const [fromEmail] = React.useState("breadandrosesjc@gmail.com");
   const [text, setText] = React.useState("");
@@ -101,148 +108,164 @@ export default function CommunicationPage() {
         <GroupRoundedIcon sx={{ width: 44, height: 44 }}></GroupRoundedIcon>
         Send Email To Volunteers
       </div>
-      <div className="flex items-start">
-        <div className="w-1/3">
-          <p className="font-semibold">Subject</p>
-        </div>
-        <div className="w-2/3">
-          <input
-            type="text"
-            placeholder="ex: Welcome to Bread & Roses!"
-            className="border border-gray-300 rounded-md p-2 w-full"
-            value={subject}
-            onChange={(e) => setSubject(e.target.value)}
-          />
-        </div>
-      </div>
+      {step === 1 && (
+        <>
+          <div className="flex items-start">
+            <div className="w-1/3">
+              <p className="font-semibold">Subject</p>
+            </div>
+            <div className="w-2/3">
+              <input
+                type="text"
+                placeholder="ex: Welcome to Bread & Roses!"
+                className="border border-gray-300 rounded-md p-2 w-full"
+                value={subject}
+                onChange={(e) => setSubject(e.target.value)}
+              />
+            </div>
+          </div>
 
-      <div className="flex items-start">
-        <div className="w-1/3">
-          <p className="font-semibold">Email From</p>
-          <p className="text-gray-500">This is the default email.</p>
-        </div>
-        <div className="w-2/3">
-          <input
-            type="text"
-            disabled
-            className="border border-gray-300 rounded-md p-2 w-full bg-gray-100 text-gray-600 cursor-not-allowed"
-            value={fromEmail}
-          />
-        </div>
-      </div>
+          <div className="flex items-start">
+            <div className="w-1/3">
+              <p className="font-semibold">Email From</p>
+              <p className="text-gray-500">This is the default email.</p>
+            </div>
+            <div className="w-2/3">
+              <input
+                type="text"
+                disabled
+                className="border border-gray-300 rounded-md p-2 w-full bg-gray-100 text-gray-600 cursor-not-allowed"
+                value={fromEmail}
+              />
+            </div>
+          </div>
 
-      <div className="flex items-start">
-        <div className="w-1/3">
-          <p className="font-semibold">Email Body</p>
-          <p className="text-gray-500">
-            This will be sent to all volunteers on the site.
-          </p>
-        </div>
-        <div className="w-2/3 flex flex-col gap-6">
-          <button
-            onClick={handleButtonClick}
-            disabled={sendMassEmailLoading}
-            className="border-2 border-gray-300 rounded-md p-4 text-center text-gray-500 cursor-pointer hover:border-teal-600"
-          >
-            <FileUploadRoundedIcon sx={{ color: "#138D8A" }} />
-            <p>
-              <span className="text-teal-600 font-semibold">
-                Click to upload
-              </span>{" "}
-              or drag and drop
-            </p>
-            {/* <p>SVG, PNG, JPG or GIF (max. 3MB)</p> */}
-          </button>
-          <input
-            type="file"
-            ref={fileInputRef}
-            onChange={handleFileChange}
-            className="hidden"
-            multiple
-          />
-          {/* {attachments?.length && (
-            <p className="text-gray-700">
-              Selected: {attachments.map((file) => file.name).join(", ")}
-            </p>
-          )} */}
-
-          {(attachments?.length ?? 0) > 0 &&
-            attachments?.map((src, index) => (
-              <div
-                key={index}
-                className="p-4 gap-4 flex flex-row items-center w-full"
+          <div className="flex items-start">
+            <div className="w-1/3">
+              <p className="font-semibold">Email Body</p>
+              <p className="text-gray-500">
+                This will be sent to all volunteers on the site.
+              </p>
+            </div>
+            <div className="w-2/3 flex flex-col gap-6">
+              <button
+                onClick={handleButtonClick}
+                disabled={sendMassEmailLoading}
+                className="border-2 border-gray-300 rounded-md p-4 text-center text-gray-500 cursor-pointer hover:border-teal-600"
               >
-                <UploadFileIcon sx={{ color: "#138D8A" }} />
-                <div className="flex flex-col">
-                  <div>{src.name}</div>
-                  <div className="text-gray-500 flex flex-row gap-2">
-                    <div>{formatFileSize(src.size)}</div>
-                    <div>•</div>
-                    {/* TODO: Actually see if its Complete */}
-                    <div>Complete</div>
+                <FileUploadRoundedIcon sx={{ color: "#138D8A" }} />
+                <p>
+                  <span className="text-teal-600 font-semibold">
+                    Click to upload
+                  </span>{" "}
+                  or drag and drop
+                </p>
+              </button>
+              <input
+                type="file"
+                ref={fileInputRef}
+                onChange={handleFileChange}
+                className="hidden"
+                multiple
+              />
+              {(attachments?.length ?? 0) > 0 &&
+                attachments?.map((src, index) => (
+                  <div
+                    key={index}
+                    className="p-4 gap-4 flex flex-row items-center w-full"
+                  >
+                    <UploadFileIcon sx={{ color: "#138D8A" }} />
+                    <div className="flex flex-col">
+                      <div>{src.name}</div>
+                      <div className="text-gray-500 flex flex-row gap-2">
+                        <div>{formatFileSize(src.size)}</div>
+                        <div>•</div>
+                        <div>Complete</div>
+                      </div>
+                    </div>
+                    <DeleteIcon
+                      onClick={() =>
+                        setAttachments(
+                          (prev) => prev?.filter((_, i) => i !== index) || []
+                        )
+                      }
+                      className="ml-auto"
+                    />
                   </div>
-                </div>
-                <DeleteIcon
-                  onClick={() =>
-                    setAttachments(
-                      (prev) => prev?.filter((_, i) => i !== index) || []
-                    )
+                ))}
+
+              <textarea
+                className="border border-gray-300 rounded-md p-2 w-full resize-none"
+                placeholder="Type your email content"
+                value={text}
+                onChange={(e) => setText(e.target.value)}
+              />
+            </div>
+          </div>
+
+          <div className="flex items-start">
+            <div className="w-1/3"></div>
+            <div className="w-2/3">
+              <button
+                className="w-[150px] font-semibold bg-teal-600 p-2.5 px-3 text-white rounded-md items-center"
+                disabled={sendMassEmailLoading}
+                onClick={async () => {
+                  const nodeMailerAttachments =
+                    await convertFilesToNodemailerAttachment(attachments);
+                  try {
+                    // Make the API call and expect a JSON response
+                    const response = (await throttledSendMassEmail(
+                      text,
+                      subject,
+                      nodeMailerAttachments
+                    )) as SendEmailResponse | undefined;
+
+                    console.log("API response:", response);
+
+                    if (response && response.code === "SUCCESS") {
+                      setStep(2);
+                    } else {
+                      console.error(
+                        "Email sending failed or response is undefined",
+                        response
+                      );
+                    }
+                  } catch (error) {
+                    console.error("Error sending email:", error);
                   }
-                  className="ml-auto"
-                />
+                }}
+              >
+                Send Email
+              </button>
+            </div>
+          </div>
+        </>
+      )}
+      {step === 2 && (
+        <>
+          <div className="gap-8 flex flex-col items-center justify-center">
+            <div className="relative flex flex-col items-center justify-center gap-8">
+              <Image
+                src="/sentMassEmail.png"
+                alt="Sent Mass Email"
+                width={400}
+                height={350}
+                objectFit="contain"
+                quality={100}
+              />
+              <div className="text-3xl font-['Kepler_Std'] font-semibold">
+                Email sent! You will receive a copy via email.
               </div>
-            ))}
-
-          {/* {preview.length > 0 &&
-            preview.map((src, index) => (
-              <img key={index} src={src} alt="OKAY" />
-            ))} */}
-
-          <textarea
-            className="border border-gray-300 rounded-md p-2 w-full resize-none"
-            placeholder="Type your email content"
-            value={text}
-            onChange={(e) => setText(e.target.value)}
-          />
-        </div>
-      </div>
-
-      <div className="flex items-start">
-        <div className="w-1/3"></div>
-        <div className="w-2/3">
-          <button
-            className="w-[150px] font-semibold bg-teal-600 p-2.5 px-3 text-white rounded-md items-center"
-            disabled={sendMassEmailLoading}
-            onClick={async () => {
-              // TODO: See if we have to throttle conversion
-              const nodeMailerAttachments =
-                await convertFilesToNodemailerAttachment(attachments);
-              // TODO: FIX TO INCLUDE SUBJECT AND BODY, ETC
-              await throttledSendMassEmail(
-                text,
-                subject,
-                nodeMailerAttachments
-              );
-            }}
-          >
-            Send Email
-          </button>
-        </div>
-      </div>
-
-      {/* <div className="text-center">
-        <div className="relative w-full h-[50vh]">
-          <Image
-            src="/empty_list.png"
-            alt="Empty List"
-            layout="fill"
-            objectFit="contain"
-          />
-        </div>
-        <div className="text-[#344054] font-['Kepler_Std'] text-3xl font-semibold mt-8">
-          Coming Soon!
-        </div>
-      </div> */}
+            </div>
+            <button
+              onClick={() => setStep(1)}
+              className="w-auto h-auto font-semibold bg-teal-600 py-2.5 px-4 text-white rounded-md items-center"
+            >
+              Back to the original page
+            </button>
+          </div>
+        </>
+      )}
     </div>
   );
 }
