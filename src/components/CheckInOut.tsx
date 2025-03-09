1d2f46c73f7cbc82c2315d03e741de35f66b72c9
wkim10 <wkim10@tufts.edu>
2025-03-09 11:22:52 -0400

    reset selected shift time

--- src/components/CheckInOut.tsx
+++ src/components/CheckInOut.tsx
@@ -148,6 +148,7 @@ export default function CheckInOutForm() {
         onClick={() => {
           setEmail("");
           setActiveButton(null);
+          setSelectedShift(null);
           setStage("initial");
         }}
       />
