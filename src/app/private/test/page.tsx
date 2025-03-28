032a569ba2eaf26ff933a54ba717c65ab5c249e8
vle04 <ngocvinale@gmail.com>
2025-03-28 18:03:21 -0400

    created modal component

---
+++ src/app/private/test/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import React, { useState } from "react";
+import GroupSignUpModal from "@components/GroupSignUpModal";
+
+export default function EventsPage() {
+    
+  return (
+    <div>
+      <GroupSignUpModal/>
+    </div>
+  );
+}
---
+++ src/components/GroupSignUpModal.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React, { useState, useRef } from "react";
+
+const GroupSignUpModal = () => {
+    return (
+        <div className="border border-gray-300 w-[596px] h-[867.03px] shadow-md rounded-xl place-content-center">
+            <div className="flex justify-center border border-gray-300 w-[556px] h-[771.03px]">
+            </div>
+        </div>
+    );
+};
+
+export default GroupSignUpModal;
