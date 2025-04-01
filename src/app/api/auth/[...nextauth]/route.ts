bed8bdbea6819a690fe55870bafb25c66ea263e1
manuel.penaa <143857167+ManuP6789@users.noreply.github.com>
2025-04-02 00:17:19 +0100

    73 backend remember me (#90)
    * add login remember me functionality
    
    Setting session expiration with nextauth instead of cookies
    
    * add handler for dynamicAge
    
    * fix consistency issue
    
    * small changes to fix types
    
    * removed unused file
    
    * removed console logs
    
    ---------
    
    Co-authored-by: wkim10 <wkim10@tufts.edu>

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,30 @@
 import NextAuth from "next-auth/next";
-import { options } from "./options";
+import { getAuthOptions } from "./options";
+import { getToken } from "next-auth/jwt";
+import { NextRequest, NextResponse } from "next/server";
+import { NextApiRequest, NextApiResponse } from "next";
 
-const handler = NextAuth(options);
+const handler = async (req: NextRequest, res: NextResponse) => {
+  let maxAge = 24 * 60 * 60;
+  const token = await getToken({
+    req,
+    secret: process.env.NEXTAUTH_SECRET as string,
+  });
+
+  // Override maxAge based on rememberMe flag
+  if (token) {
+    maxAge = token.rememberMe ? 30 * 24 * 60 * 60 : 24 * 60 * 60;
+    const remaining = (token.exp as number) - Math.floor(Date.now() / 1000);
+    maxAge = Math.min(maxAge, remaining);
+  }
+  
+  const authOptions = getAuthOptions(maxAge);
+
+  return NextAuth(
+    req as unknown as NextApiRequest,
+    res as unknown as NextApiResponse,
+    authOptions
+  );
+};
 
 export { handler as GET, handler as POST };
