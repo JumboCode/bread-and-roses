faadd1ad4f685ca039c8f240cd99ba13def2eaac
vle04 <ngocvinale@gmail.com>
2025-04-05 23:07:59 -0400

    added time section
    created a "select date" field where a calendar will pop up/drop down from the text box when users enter the field or click on the calendar icon. clicking on the icon again or clicking outside the calendar will close it.

--- src/components/GroupSignUpModal.tsx
+++ src/components/GroupSignUpModal.tsx
@@ -1,10 +1,33 @@
 "use client";
 
 import HighlightOffIcon from '@mui/icons-material/HighlightOff';
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
+import TextField from "@mui/material/TextField";
+import { Icon } from "@iconify/react/dist/iconify.js";
+import { InputAdornment } from '@mui/material';
+import { Calendar } from "@components/Calendar";
+import "react-day-picker/style.css";
 
 const GroupSignUpModal = () => {
     const [showModal, setShowModal] = useState(true);
+    const [showCalendar, setShowCalendar] = useState(false);
+    const buttonRef = useRef(null);
+    const calendarRef = useRef(null);
+
+    useEffect(() => {
+        function handleClickOutside(event: MouseEvent) {
+            // if user clicks outside the calendar, close it
+            if (calendarRef.current && !calendarRef.current.contains(event.target)) {
+                setShowCalendar(false);
+            }
+        }
+        if (showCalendar) {
+            document.addEventListener("mousedown", handleClickOutside);
+        };
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, [showCalendar]);
 
     if (!showModal) return null;
 
@@ -24,7 +47,45 @@ const GroupSignUpModal = () => {
                 </div>
                 <hr className="my-12 h-0.5 border-t-0 bg-neutral-100 dark:bg-gray/400" />
 
-                
+                <label className="block text-sm font-medium mt-2 mb-2">Time</label> 
+                <div ref={calendarRef}>
+                <TextField
+                    className="border border-gray-300 rounded-md px-3 py-2 w-full mb-4 focus:outline-none focus:ring-1 focus:ring-blue-500"
+                    placeholder="MM/DD/YYYY"
+                    variant="outlined"
+                    label="Select Date"
+                    autoComplete='off'
+                    onFocus={() => setShowCalendar(!showCalendar)}
+                    slotProps={{
+                        input: {
+                            endAdornment: (
+                                <InputAdornment position="end">
+                                    <button
+                                        ref={buttonRef}
+                                        onClick={() => setShowCalendar(!showCalendar)}
+                                    >
+                                        <Icon icon={"mdi:calendar"} width="25"/>
+                                    </button>
+                                </InputAdornment>
+                            )
+                        }
+                    }}
+                />
+                {showCalendar && (
+                    <div
+                        style={{
+                            position: "absolute",
+                            top: "calc(100% - 620px)",
+                            left: "20px", 
+                            zIndex: 10,
+                            backgroundColor: "white",
+                            borderRadius: "8px",
+                            boxShadow: "0 2px 10px rgba(0, 0, 0, 0.1)",
+                        }}>
+                        <Calendar/>
+                    </div>
+                )}
+                </div>
 
                 <label className="block text-sm font-medium mt-2 mb-2">Name of group</label>
                 <input
